6b62f5984c116b55cbad6bcb5236d3004a144974
taan234 <quijanotania@gmail.com>
2024-07-15 18:42:02 -0600

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,14 +5,10 @@ import styles from './Home.module.css';
 function Home() {
   return (
     <div className={styles.home}>
-      <h2 className={styles.home__title}>Bienvenido a Nuestra Tienda</h2>
-      <p className={styles.home__text}>Encuentra los mejores productos ferreteros aquí.</p>
-      <p className={styles.home__text}>
-        Ofrecemos una amplia gama de herramientas, materiales de construcción y productos de ferretería para satisfacer todas tus necesidades.
-      </p>
-      <p className={styles.home__text}>
-        Nuestro objetivo es proporcionarte productos de alta calidad a precios competitivos. ¡Explora nuestro catálogo y descubre todo lo que tenemos para ofrecerte!
-      </p>
+      <h2 className={styles.home__title}>Descubre las últimas tendencias en moda</h2>
+      <p className={styles.home__text}>Explora nuestra colección primavera-verano y encuentra tu estilo único.</p>
+      <p className={styles.home__text}>Desde ropa casual hasta elegantes prendas de noche, tenemos todo lo que necesitas.</p>
+      <p className={styles.home__text}>Visítanos y deja que nuestra moda hable por ti.</p>
     </div>
   );
 }
